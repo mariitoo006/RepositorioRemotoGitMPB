866ee468d09619732659c0d3324079d552b39097
FusionRamas <Mario@DESKTOP-GRU9AR0>
2026-02-24 16:00:43 +0100

    Segundo Commit.

--- Proyecto/src/Paquete/Principal.java
+++ Proyecto/src/Paquete/Principal.java
@@ -3,6 +3,10 @@ package Paquete;
 public class Principal {
 	public static void main (String [] args) {
 		System.out.print("Hola Mundo");
+		
+		/**Mensajes de rama1**/
+		System.out.print("Primer mensaje de la rama1");
+		System.out.print("Segundo mensaje de la rama1");
 	}
 
 }
